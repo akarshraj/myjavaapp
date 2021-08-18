9948d40195086605eda9a1935b8e60bd8c162fa2
Akarsh <raj.akarsh1999@gmail.com>
2021-08-18 16:19:25 +0530

    'This is the 2nd version'

--- myjavaapp/src/om/ofss/HelloWorld.java
+++ myjavaapp/src/om/ofss/HelloWorld.java
@@ -1,10 +1,15 @@
 package om.ofss;
 
 public class HelloWorld {
-
+	
+	public static int add(int x, int y) {
+		return x+y;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Abc");
+		System.out.println(add(2, 5));
 	}
 
 }
